5aa9f28e1945dd8d8937be19c68501246a4a3f18
jth55 <jthince55@gmail.com>
2020-04-19 03:20:10 -0500

    Added baseline for command description. Fixed JS flaws in game logic.

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ app = Flask(__name__)
 def home():
 	ran = random.randint(0, 2)
 	files = ['files/iptables.txt', 'files/script.txt', 'files/misc.txt']
+	descFiles = ['files/iptablesDesc.txt', 'files/scriptDesc.txt', 'files/miscDesc.txt']
 	with open(files[ran]) as f:
 	    bloc = f.readlines()
+	with open(descFiles[ran]) as f:
+	    desc = f.readlines()
 
-	return render_template("index.html", bloc=bloc)
+	return render_template("index.html", bloc=bloc, desc=desc)
     
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
